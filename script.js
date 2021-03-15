32a47e845e3cbe3575bae8dba917373bbdbe124d
Priyanshu <priyanshu9943@gmail.com>
2021-03-15 17:13:16 +0530

    links

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 const projects = [
   {
-    link: "#",
+    link: "https://codepen.io/Priyanshu180/full/eYBGjEv",
     src: "https://drive.google.com/uc?id=152U8RmU1m_Zq73RTv9c4873hv_aGVHk8",
-    name: "Project 1"
+    name: "Learning App"
   }, {
-    link: "#",
+    link: "https://codepen.io/Priyanshu180/full/vYygeKq",
     src: "https://drive.google.com/uc?id=1UGesAaXp0iZ6V2gVn3ZHPWZbVOk2-C6k",
-    name: "Project 2"
+    name: "Anime Video Player"
   }, {
-    link: "#",
+    link: "https://codepen.io/Priyanshu180/full/dyOGqMy",
     src: "https://drive.google.com/uc?id=1J0_l9b2CuGq-h6oGUn-wPTLZMxiRJury",
-    name: "Project 3"
+    name: "Anime Song Player"
   }, {
-    link: "#",
+    link: "https://codepen.io/Priyanshu180/full/abmraBQ",
     src: "https://drive.google.com/uc?id=1BFFsJpjm6PJIdN2ZBwmP1zvCkcVwcWFh",
-    name: "Project 4"
+    name: "Maths Quiz"
   }, {
-    link: "#",
+    link: "https://codepen.io/Priyanshu180/full/NWbMGdy",
     src: "https://drive.google.com/uc?id=1ho_MkHqY_KW-MXQ0iYbE4SYxTBy1YFsP",
-    name: "Project 5"
+    name: "Piano Tiles"
   }, {
-    link: "#",
+    link: "https://codepen.io/Priyanshu180/full/RwGjjjV",
     src: "https://drive.google.com/uc?id=1HpQz7cSzgLaIft32RQruOhtcimMG0ud-",
-    name: "Project 6"
+    name: "Day Finder Application"
   }, {
-    link: "#",
+    link: "https://codepen.io/Priyanshu180/full/YzGwgYE",
     src: "https://drive.google.com/uc?id=1R3hkma5qhCiAyzWDomptebagsKo60T8J",
-    name: "Project 7"
+    name: "Tribe page"
   }, {
     link: "#",
     src: "https://drive.google.com/uc?id=1AeIyNFm3kW-yyjBqjScJ_IrJihkwrxvk",
@@ -36,6 +36,6 @@ const projects = [
 
 $(function () {
   for (var a = 0; a < projects.length; a++){
-    $('#proj-list').append('<div class="col-md-6"><a href="' + projects[a].link + '"><div class="project-ui"><img class="usn" src="'+ projects[a].src +'" alt="'+ projects[a].name +'"><span class="pd">'+ projects[a].name +'</span></div></a></div>');
+    $('#proj-list').append('<div class="col-md-6"><a href="' + projects[a].link + '"><div class="project-ui"><img class="usn" src="'+ projects[a].src +'" alt="'+ projects[a].name +'"><br><span class="pd">'+ projects[a].name +'</span></div></a></div>');
   }
 });
